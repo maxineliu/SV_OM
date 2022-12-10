cb6820e2c8020face8d9262e15d1eff99f5b23bd
maxineliu <rliu13@uoguelph.ca>
2022-12-10 02:50:58 -0500

    Sort by fst column in descending order

--- vcftools/2.manhattan_density.R
+++ vcftools/2.manhattan_density.R
@@ -14,8 +14,8 @@ library(CMplot)
 library(dplyr)
 
 # Setting variables
-input_F <- "/Users/maxineliu/work/bufo/outlier_methods/VCFtools/results_plots/12bufo.DUP.filtered.weir.fst" #fst file
-input_ID <- "/Users/maxineliu/work/bufo/outlier_methods/VCFtools/results_plots/12bufo.DUP.filtered_ID.txt" # ID list
+input_F <- "/Users/maxineliu/work/bufo/outlier_methods/Sniffles/VCFtools/results_plots/12bufo.DUP.filtered.weir.fst" #fst file
+input_ID <- "/Users/maxineliu/work/bufo/outlier_methods/Sniffles/VCFtools/results_plots/12bufo.DUP.filtered_ID.txt" # ID list
 # cutoff in line 36
 # n in line 38
 # file name in line 40
@@ -38,7 +38,7 @@ length(which(Fst$val > cutoff)) #outlier个数
 outlier <- Fst %>% slice_max(val, n=14) #return rows with the biggest N Fst
 ID_outlier <- outlier[,1]
 write(ID_outlier, file = "/Users/maxineliu/work/bufo/outlier_methods/VCFtools/12bufo.DUP.filtered.outlier.id", sep = "\n") # extract outlier records from VCF
-write.table(outlier, file = "/Users/maxineliu/work/bufo/outlier_methods/VCFtools/12bufo.DUP.filtered.outlier.weir.fst", sep = "\t", eol = "\n")
+write.table(arrange(outlier, desc(val)), file = "/Users/maxineliu/work/bufo/outlier_methods/VCFtools/12bufo.DUP.filtered.outlier.weir.fst", sep = "\t", eol = "\n", row.names = F, quote = F)
 chr_name <- c("NC_058080.1", "NC_058081.1", "NC_058082.1", "NC_058083.1", "NC_058084.1", "NC_058085.1", "NC_058086.1", "NC_058087.1", "NC_058088.1", "NC_058089.1", "NC_058090.1", "NC_008410.1")
 # subset <- c("Fst1",	"Fst2",	"Fst3",	"Fst4",	"Fst5", "Fst6", "Fst7", "Fst8", "Fst9", "Fst10", "Fst11", "FstMito")
 
